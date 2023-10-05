b7a7e00bcfa7c56d7181688311d4b205d411cd44
Matheus Guimaraes <matheusguimaraes668@gmail.com>
2023-10-04 22:26:46 -0300

    adicionado controllers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import {Router, Request, Response} from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('home');
-})
+router.get('/', PageController.home);
+router.get('/dogs', PageController.dogs);
+router.get('/cats', PageController.cats);
+router.get('/fishes', PageController.fishes);
+
+router.get('search', SearchController.search)
+
 
 
 
